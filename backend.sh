0cdf076f63b5e965d1cea4489e8999d5287b3dcd
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-13 13:31:11 -0500

    try to implement echo command

--- backend.sh
+++ backend.sh
@@ -1,19 +1,41 @@
+echo disable the nodejs version
 dnf module disable nodejs -y
+
+echo enable the nodejs 18 version
 dnf module enable nodejs:18 -y
 
+echo install the nodejs
 dnf install nodejs -y
+
+echo configure the backend service
 cp backend.service /etc/systemd/system/backend.service
 
+echo adding the user
 useradd expense
+
+echo removing the default content
 rm -rf /app
+
+echo creating the directory
 mkdir /app
+
+echo download the application content
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 
+echo extracting application content
 unzip /tmp/backend.zip
+
+echo downloading the application dependencies
 npm install
+
+echo relaoding the systemd and start the backend srevice
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
+
+echo install mysql client
 dnf install mysql -y
+
+echo load schema
 mysql -h mysql.madhanmohanreddy.tech -uroot -pExpenseApp@1 < /app/schema/backend.sql

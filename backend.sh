78702a84d405b50e1192b5c52ecdc7c083c8c712
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-14 17:18:06 -0500

    special variables

--- backend.sh
+++ backend.sh
@@ -1,3 +1,4 @@
+$1
 log_file=/tmp/expense.log
 echo -e "\e[36mdisable the nodejs\e[0m"
 dnf module disable nodejs -y &>>$log_file
@@ -38,4 +39,4 @@ echo -e "\e[36m install the mysql client\e[0m"
 dnf install mysql -y &>>$log_file
 
 echo -e "\e[36m reload the schema\e[0m"
-mysql -h mysql.madhanmohanreddy.tech -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+mysql -h mysql.madhanmohanreddy.tech -uroot -p${1} < /app/schema/backend.sql &>>$log_file

0bb8beec6bd0f87006a0ea9761853111959d15c9
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-13 21:21:47 -0500

    how to access the variable

--- backend.sh
+++ backend.sh
@@ -1,41 +1,41 @@
+log_file=/tmp/expense.log
 echo -e "\e[36mdisable the nodejs\e[0m"
-dnf module disable nodejs -y &>>/tmp/expense.log
-
+dnf module disable nodejs -y &>>$log_file
 echo -e "\e[36menbale the nodejs\e[0m"
-dnf module enable nodejs:18 -y &>>/tmp/expense.log
+dnf module enable nodejs:18 -y &>>$log_file
 
 echo -e "\e[36minstall the nodejs\e[0m"
-dnf install nodejs -y &>>/tmp/expense.log
+dnf install nodejs -y &>>$log_file
 
 echo -e "\e[36mconfigure the backend service\e[0m"
-cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 echo -e "\e[36madding the user\e[0m"
-useradd expense &>>/tmp/expense.log
+useradd expense &>>$log_file
 
 echo -e "\e[36mremoving the default content\e[0m"
-rm -rf /app &>>/tmp/expense.log
+rm -rf /app &>>$log_file
 
 echo -e "\e[36mcreating the directory\e[0m"
-mkdir /app &>>/tmp/expense.log
+mkdir /app &>>$log_file
 
 echo -e "\e[36mdownload the backend content\e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>/tmp/expense.log
-cd /app &>>/tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
+cd /app &>>$log_file
 
 echo -e "\e[36mextract the backend content\e[0m"
-unzip /tmp/backend.zip &>>/tmp/expense.log
+unzip /tmp/backend.zip &>>$log_file
 
 echo -e "\e[36minstall the required packages\e[0m"
-npm install &>>/tmp/expense.log
+npm install &>>$log_file
 
 echo -e "\e[36mreload the systemd and start the service"
-systemctl daemon-reload &>>/tmp/expense.log
-systemctl enable backend &>>/tmp/expense.log
-systemctl start backend &>>/tmp/expense.log
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 
 echo -e "\e[36m install the mysql client\e[0m"
-dnf install mysql -y &>>/tmp/expense.log
+dnf install mysql -y &>>$log_file
 
 echo -e "\e[36m reload the schema\e[0m"
-mysql -h mysql.madhanmohanreddy.tech -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
+mysql -h mysql.madhanmohanreddy.tech -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file

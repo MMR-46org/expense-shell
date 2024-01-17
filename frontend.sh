9bbc1d499d385c68ed4e546a9db825ac4fb93319
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-17 11:06:07 -0500

    implement the log files

--- frontend.sh
+++ frontend.sh
@@ -5,25 +5,25 @@ head(){
 }
 
 head "install nginx"
-dnf install nginx -y &>>log_file
+dnf install nginx -y &>>$log_file
 echo $?
 head "copy the expense configuration file"
-cp expense.conf /etc/nginx/default.d/expense.conf  &>>log_file
+cp expense.conf /etc/nginx/default.d/expense.conf  &>>$log_file
 echo $?
 
 head "remove the default content"
-rm -rf /usr/share/nginx/html/* &>>log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 echo $?
 head "download the expense content"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 echo $?
 head "change the directory"
-cd /usr/share/nginx/html &>>log_file
+cd /usr/share/nginx/html &>>$log_file
 echo $?
 head "extract the expense content"
-unzip /tmp/frontend.zip &>>log_file
+unzip /tmp/frontend.zip &>>$log_file
 echo $?
 head "enable and start nginx"
-systemctl enable nginx &>>log_file
-systemctl restart nginx &>>log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 echo $?

5d56ce8e3a03eeaaa91c2f21c5163687508218eb
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-17 10:42:24 -0500

    implement the exit status code

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,29 @@
-dnf install nginx -y
-cp expense.conf /etc/nginx/default.d/expense.conf
+log_file=/tmp/expense.log
 
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+head(){
+  echo -e "\e[36m$1\e[0m"
+}
 
-systemctl enable nginx
-systemctl restart nginx
+head "install nginx"
+dnf install nginx -y &>>log_file
+echo $?
+head "copy the expense configuration file"
+cp expense.conf /etc/nginx/default.d/expense.conf  &>>log_file
+echo $?
+
+head "remove the default content"
+rm -rf /usr/share/nginx/html/* &>>log_file
+echo $?
+head "download the expense content"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+echo $?
+head "change the directory"
+cd /usr/share/nginx/html &>>log_file
+echo $?
+head "extract the expense content"
+unzip /tmp/frontend.zip &>>log_file
+echo $?
+head "enable and start nginx"
+systemctl enable nginx &>>log_file
+systemctl restart nginx &>>log_file
+echo $?

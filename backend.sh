5fda8ab295689bb5c483f92f5f231feb133f6780
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-19 14:51:26 -0500

    implement number checks

--- backend.sh
+++ backend.sh
@@ -6,40 +6,85 @@ source common.sh
 
 head "disable the nodejs"
 dnf module disable nodejs -y &>>log_file
-head $?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 head "enable the nodejs"
 dnf module enable nodejs:18 -y &>>log_file
-head $?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 
 head "install the nodejs"
 dnf install nodejs -y &>>log_file
-head $?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 head "config the backend service"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
-head $?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 head "adding the user"
 useradd expense &>>log_file
-head $?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 
 head "install the software"
 npm install &>>log_file
-head $?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 head "reload the restart the backend"
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
-head $?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 head "install mysql"
 dnf install mysql -y &>>log_file
-head &?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi
 
 head "reload the schema"
 mysql -h mysql.madhanmohanreddy.tech -uroot -p${mysql_password} < /app/schema/backend.sql &>>log_file
-head $?
+if [ $? -eq 0 ];then
+  echo SUCCESS
+else
+  echo FAILURE
+  exit 1
+fi

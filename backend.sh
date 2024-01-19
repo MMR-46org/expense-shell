6c5fec95e9d5bc72ce75540573f15006aad1b91d
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-19 11:36:32 -0500

    shell script implementing

--- backend.sh
+++ backend.sh
@@ -3,42 +3,43 @@ component=backend
 
 source common.sh
 
+
 head "disable the nodejs"
 dnf module disable nodejs -y &>>log_file
-echo $?
+head $?
 
 head "enable the nodejs"
 dnf module enable nodejs:18 -y &>>log_file
-echo $?
+head $?
+
 
 head "install the nodejs"
 dnf install nodejs -y &>>log_file
-echo $?
+head $?
 
-head "configure the backend service"
+head "config the backend service"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
-echo $?
+head $?
 
-head "adding user"
+head "adding the user"
 useradd expense &>>log_file
-echo $?
+head $?
 
-app_pre "/app"
 
-head "install npm"
+head "install the software"
 npm install &>>log_file
-echo $?
+head $?
 
-head "enable and start the backend"
+head "reload the restart the backend"
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
-echo $?
+head $?
 
 head "install mysql"
 dnf install mysql -y &>>log_file
-echo $?
+head &?
 
-head "loading schema"
+head "reload the schema"
 mysql -h mysql.madhanmohanreddy.tech -uroot -p${mysql_password} < /app/schema/backend.sql &>>log_file
-echo $?
+head $?
